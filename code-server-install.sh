1e7fe1832cb3fad6442ae4002500786d79e667b0
luckyway7 <119154086+luckyway7@users.noreply.github.com>
2025-12-25 13:02:18 +0300

    Update code-server-install.sh

--- code-server-install.sh
+++ code-server-install.sh
@@ -10,17 +10,17 @@ CODE_SERVER_PASSWORD=$(whiptail --passwordbox "[code-server] пароль" 8 50 
 curl -fsSL https://code-server.dev/install.sh | bash
 
 CODE_SERVER_BIN=/usr/bin/code-server
+SERVICE_USER=$(whoami)
+USER_HOME=$(getent passwd "$SERVICE_USER" | cut -d: -f6)
 
-mkdir -p ~/.config/code-server
+mkdir -p "$USER_HOME/.config/code-server"
 
-cat > ~/.config/code-server/config.yaml <<EOF
+cat > "$USER_HOME/.config/code-server/config.yaml" <<EOF
 bind-addr: 0.0.0.0:$CODE_SERVER_PORT
 auth: password
 password: $CODE_SERVER_PASSWORD
 cert: false
 EOF
-
-SERVICE_USER=$(whoami)
 
 sudo tee /etc/systemd/system/code-server.service > /dev/null <<EOF
 [Unit]
@@ -30,8 +30,6 @@ After=network.target
 [Service]
 Type=simple
 User=$SERVICE_USER
-WorkingDirectory=$HOME
-Environment="SHELL=/usr/bin/bash"
 ExecStart=$CODE_SERVER_BIN
 Restart=on-failure
 
@@ -41,10 +39,9 @@ EOF
 
 sudo systemctl daemon-reload
 sudo systemctl enable code-server
-sudo systemctl start code-server
 
-mkdir -p /root/.local/share/code-server/User
-cat > /root/.local/share/code-server/User/settings.json <<'EOF'
+mkdir -p "$USER_HOME/.local/share/code-server/User"
+cat > "$USER_HOME/.local/share/code-server/User/settings.json" <<'EOF'
 {
     "window.menuBarVisibility": "classic",
     "workbench.editor.editorActionsLocation": "titleBar",
@@ -77,6 +74,6 @@ sudo jq '. + {
   }
 }' /usr/lib/code-server/lib/vscode/product.json | sudo sponge /usr/lib/code-server/lib/vscode/product.json
 
-/usr/bin/code-server --install-extension nextbook.file-and-folder-icons
+$CODE_SERVER_BIN --install-extension nextbook.file-and-folder-icons
 
 sudo systemctl restart code-server
